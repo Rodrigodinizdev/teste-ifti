3f8d1863f26681651094de7c0ed09e579c525e0c
miguizin <miguelpadua161@gmail.com>
2026-09-24 12:12:45 -0300

    feat: Adiciona operacoes DML do desafio de SQL

---
+++ respostas/miguel-padua/desafio-sql/Q3.sql
@@ -0,0 +1,6 @@
+use LocadoraIfti
+GO
+
+INSERT INTO Reserva (IdVeiculo, IdCliente, DataReserva, StatusReserva, DataAtendimento)
+VALUES (3, 12, '2026-07-20', 'Ativa', NULL), (8, 7, '2026-07-20', 'Ativa', NULL ), (4, 25, '2026-07-20', 'Ativa', NULL);
+
---
+++ respostas/miguel-padua/desafio-sql/Q4.sql
@@ -0,0 +1,6 @@
+use LocadoraIfti
+GO
+
+UPDATE Reserva
+SET DataAtendimento = '2026-07-20', StatusReserva = 'Atendida'
+WHERE Id = 2;
